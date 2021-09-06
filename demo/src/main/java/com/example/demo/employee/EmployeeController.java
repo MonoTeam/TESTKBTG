ec51d3e4eb0b970f15877b1ce8d4259ffe8ed64c
Manassawee <winra159@gmail.com>
2021-09-06 15:40:28 +0700

    'ws05'

--- demo/src/main/java/com/example/demo/employee/EmployeeController.java
+++ demo/src/main/java/com/example/demo/employee/EmployeeController.java
@@ -1,20 +1,30 @@
 package com.example.demo.employee;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EmployeeController {
 
+    @GetMapping("/employees/{id}")
+    public EmployeeResponse getEmployeeById(@PathVariable String id) {
+//       Validate input
+//       Claening data
+        EmployeeResponse employee01 = new EmployeeResponse();
+        employee01.setId(Integer.parseInt(id));
+        employee01.setName("somkiat");
+        return employee01;
+    }
+
     @GetMapping("/employees")
     public EmployeeResponse[] listEmployee() {
-    	EmployeeResponse ep1 = new EmployeeResponse();
-    	ep1.setId(1);
-    	ep1.setName("YES");
-    	EmployeeResponse ep2 = new EmployeeResponse();
-    	ep2.setId(2);
-    	ep2.setName("No");
-        return new EmployeeResponse[]{ep1,ep2};
+        EmployeeResponse employee01 = new EmployeeResponse();
+        employee01.setId(1);
+        employee01.setName("somkiat");
+        EmployeeResponse employee02 = new EmployeeResponse();
+        employee02.setId(2);
+        employee02.setName("somkiat 2");
+        return new EmployeeResponse[]{employee01, employee02};
     }
-    
 }
