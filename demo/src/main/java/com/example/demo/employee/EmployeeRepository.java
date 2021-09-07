cd4f6490320c80b38a215d58adbb9dd50065de88
Manassawee <winra159@gmail.com>
2021-09-07 10:32:19 +0700

    'repo'

---
+++ demo/src/main/java/com/example/demo/employee/EmployeeRepository.java
@@ -0,0 +1,16 @@
+package com.example.demo.employee;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface EmployeeRepository
+        extends JpaRepository<Employee, Integer> {
+
+//    List<Employee> findAllByIdAndName(int id,String name);
+    // select * from employee where id=? ans name=?
+
+
+}
+
---
+++ demo/src/main/java/com/example/demo/employee/EmployeeService.java
@@ -0,0 +1,24 @@
+package com.example.demo.employee;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.Optional;
+
+@Service
+public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    public EmployeeResponse getById(int id) {
+        Optional<Employee> result = employeeRepository.findById(id);
+        if(result.isPresent()) {
+            // Map data from entity to response
+            EmployeeResponse response = new EmployeeResponse();
+            response.setId(result.get().getId());
+            response.setName(result.get().getName());
+            return response;
+        }
+        throw new RuntimeException("Employee not found id=" + id);
+    }
+}
